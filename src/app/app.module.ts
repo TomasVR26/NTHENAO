9ae135b84ef9b8020d6ab95d623a0aa1bf834094
unknown <SenaTarde@ARMAPRRQDFSD214.FormacionSena.RED>
2024-04-25 19:01:20 -0500

    proyecto angular 6 trimestre

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { AboutComponent } from './components/about/about.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardProductComponent,
+    HeaderComponent,
+    CatalogoComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+// services/api.service.ts
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getProducts() {
+    return this.http.get<any[]>(`http://localhost:8000/products`);
+  }
+}
